d8c81fdea8ea2107fc585c5a21e1532134152636
tony-yakim <tonyprideman@gmail.com>
2026-04-29 20:15:35 +0200

    Use Joy's photo as app icon and avatar

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'joys-diary-v3';
+const CACHE = 'joys-diary-v4';
 const ASSETS = [
   '/',
   '/index.html',
@@ -7,6 +7,7 @@ const ASSETS = [
   '/manifest.json',
   '/icons/icon-192.png',
   '/icons/icon-512.png',
+  '/icons/joy.jpg',
 ];
 
 self.addEventListener('install', e => {
